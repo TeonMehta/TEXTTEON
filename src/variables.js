49252cbd80a77e9b62e65cc0e2cfdd6feb206761
TeonMehta <Teonmehta@gmail.com>
2020-02-18 18:01:47 -0500

    initial commit. Dev server set up.

---
+++ src/variables.js
@@ -0,0 +1,44 @@
+// VALUES
+
+
+
+// VARIABLES - BINDINGS
+
+
+
+// STRINGS
+
+var string = "This is a string of text";
+var string2 = 'this is a string with single quotes';
+var string3 = `this is a string with backticks`;
+var string_newline =   `this will be on a \n new line`;
+var template_literal = `this can calculate half of 100 is ${100/2} `;
+
+
+
+// NUMBERS
+var integer = 10;
+var float = 10.0;
+
+// ARITHMETIC
+
+var a = 1;
+var b = 2;
+
+function add(a,b) {
+let sum = a + b;
+return sum;
+}
+
+
+
+
+
+
+// BOOLEANS
+var boolean = true;
+var nothing = null;
+
+
+
+console.log(integer);
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        app:            './src/index.js',
+        // variables:      './src/variables.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "Webpack App"
+        }),
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            {
+              test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test:  /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ],
+    },
+};
